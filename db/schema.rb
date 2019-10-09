ab40020d0087c4c8d7aee81a1b48649140c311cd
maisonn <37782167+maisonn@users.noreply.github.com>
2019-10-09 14:43:49 +0300

    Added Image Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_175015) do
+ActiveRecord::Schema.define(version: 2019_10_09_102221) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "kind"
   end
 
 end
